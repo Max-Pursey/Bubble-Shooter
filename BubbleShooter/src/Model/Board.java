8a9ecf6d73a63019595438d586b768fd0a28de1f
Max-Pursey <maxwell.pursey@sjsu.edu>
2019-10-29 20:54:47 -0700

    I have implemented the board class.

--- BubbleShooter/src/Model/Board.java
+++ BubbleShooter/src/Model/Board.java
@@ -1,7 +1,5 @@
 package Model;
 
-import java.awt.Graphics2D;
-
 import Controller.Controller;
 
 /**
@@ -11,25 +9,37 @@ import Controller.Controller;
 public final class Board {
 
 	private static final Board INSTANCE = new Board();
+	public Bubble[][] board;
 	
 	/**
-	 * Constructs a Board object that holds the bubbles and the bubble shooter.
-	 * Creating a new board restarts the game by creating the field of random bubbles
-	 * and a new bubble shooter
+	 * Constructs a Board object that holds the grid of bubbles.
 	 */
 	private Board() {
-		Controller shooter = new Controller();
+		board = new Bubble[5][5]; 
+		for(int x = 0; x < 5; x++)
+		{
+			Bubble bubble = new Bubble(board[x-1][1], null, null, null, null, null);	
+			board[x][1] = bubble;
+		}
+		for(int y = 1; y < 3; y++)
+		{
+			for(int x = 0; x < 5; x++)
+			{
+				if(y % 2 == 0)
+				{
+					Bubble bubble = new Bubble(board[x-1][y], null, null, board[x+1][y-1], null, null);	
+					board[x][y] = bubble;
+				}
+				else
+				{
+					Bubble bubble = new Bubble(board[x-1][y], null, board[x][y-1], board[x+1][y-1], null, null);	
+					board[x][y] = bubble;
+				}
+			}
+		}
 	}
 	
 	public static Board getInstance() {
 		return INSTANCE;
-	}
-	
-	/**
-	 *  Paint components of the board 
-	 *  @param g the Graphics2D object
-	 */
-	public void paint(Graphics2D g) {
-		
 	}
 }

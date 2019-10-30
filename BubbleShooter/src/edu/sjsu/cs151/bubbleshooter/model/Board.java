99424080b43513e7a4ecb769afe5ecf7e080ba42
Max-Pursey <maxwell.pursey@sjsu.edu>
2019-10-30 16:37:21 -0700

    Fixed board to have the edge case that I thought I had already added

--- BubbleShooter/src/edu/sjsu/cs151/bubbleshooter/model/Board.java
+++ BubbleShooter/src/edu/sjsu/cs151/bubbleshooter/model/Board.java
@@ -18,8 +18,16 @@ public final class Board {
 		board = new Bubble[5][5]; 
 		for(int x = 0; x < 5; x++)
 		{
-			Bubble bubble = new Bubble(board[x-1][1], null, null, null, null, null);	
-			board[x][1] = bubble;
+			if(x == 0)
+			{
+				Bubble bubble = new Bubble(null, null, null, null, null, null);	
+				board[x][1] = bubble;
+			}
+			else
+			{
+				Bubble bubble = new Bubble(board[x-1][1], null, null, null, null, null);	
+				board[x][1] = bubble;
+			}
 		}
 		for(int y = 1; y < 3; y++)
 		{

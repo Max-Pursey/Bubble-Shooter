81e1e0aa7cf45d95452275909531905b752110da
greg <gregory.vansciver@sjsu.edu>
2019-12-04 00:41:57 -0800

    FireBubbleValve work in progress

--- BubbleShooter/src/edu/sjsu/cs151/bubbleshooter/controller/FireBubbleValve.java
+++ BubbleShooter/src/edu/sjsu/cs151/bubbleshooter/controller/FireBubbleValve.java
@@ -4,16 +4,59 @@ import java.awt.MouseInfo;
 import java.awt.Point;
 import java.awt.PointerInfo;
 
+import edu.sjsu.cs151.bubbleshooter.model.*;
+
 public class FireBubbleValve implements Valve {
 	public ValveResponse execute(Message message) {
 		if(message.getClass() != FireBubbleMessage.class) {
 			return ValveResponse.MISS;
 		}
 		PointerInfo a = MouseInfo.getPointerInfo();
-		Point b = a.getLocation();
-		System.out.println((int) b.getX() + ", " + (int)b.getY());
+		Point p = a.getLocation();
+		x = p.getX() - 415;
+		y = (p.getY() - 495) * -1 ;
+		
+		
+		System.out.println(x + ", " + y);
+		double q = Math.sqrt((x*x)+(y*y));
+		dx = x/q;
+		dy = y/q;
+		System.out.println(dx + ", " + dy);
+		GameInfo gi = new GameInfo();
+		Bubble fired = gi.getAmmo().get(gi.getAmmo().size());
+		Bubble[][] board = gi.getBoardInfo();
+		fired.dx = dx;
+		fired.dy = dy;
+		CollisionVisitor cv = new CollisionVisitor(fired);
+		
+		while(cv.getCollision() != null || fired.y <= 0) {
 		// actions in model
-		// actions in view
+		for(Bubble[] bs : board)
+		{
+			for(Bubble b : bs) {
+				cv.visitBubble(b);
+			}
+		}
+		
+		// update view
+			
+		// increment bubble's location
+		
+		// if x is out of boundaries, flip the dx
+		}
+		
+		// set bubble x and y to appropriate final locations
+		
+		// set bubble dx, dy to 0
+		
+		// update view
+		
+		
 		return ValveResponse.EXECUTED;
 	}
+	
+	private double x;
+	private double y;
+	private double dx;
+	private double dy;
 }

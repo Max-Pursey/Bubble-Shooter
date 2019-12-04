d44f15ca363163b4d9a86f8d9221558379260f2c
greg <gregory.vansciver@sjsu.edu>
2019-12-03 23:22:24 -0800

    test push

--- BubbleShooter/src/edu/sjsu/cs151/bubbleshooter/controller/FireBubbleValve.java
+++ BubbleShooter/src/edu/sjsu/cs151/bubbleshooter/controller/FireBubbleValve.java
@@ -1,10 +1,17 @@
 package edu.sjsu.cs151.bubbleshooter.controller;
 
+import java.awt.MouseInfo;
+import java.awt.Point;
+import java.awt.PointerInfo;
+
 public class FireBubbleValve implements Valve {
 	public ValveResponse execute(Message message) {
 		if(message.getClass() != FireBubbleMessage.class) {
 			return ValveResponse.MISS;
 		}
+		PointerInfo a = MouseInfo.getPointerInfo();
+		Point b = a.getLocation();
+		System.out.println((int) b.getX() + ", " + (int)b.getY());
 		// actions in model
 		// actions in view
 		return ValveResponse.EXECUTED;

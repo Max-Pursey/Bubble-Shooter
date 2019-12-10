279193906b2000b4f06a87bae44b4d9189d02a92
Max-Pursey <maxwell.pursey@sjsu.edu>
2019-12-09 21:09:34 -0800

    Removed scoreboard
    we wont be able to implement it in time

--- BubbleShooter/src/edu/sjsu/cs151/bubbleshooter/view/View.java
+++ BubbleShooter/src/edu/sjsu/cs151/bubbleshooter/view/View.java
@@ -17,9 +17,7 @@ public class View extends JFrame{
 	private static JFrame frame;
 	private static JPanel welcomeAnim;
 	private static JPanel gameView;
-	private static JPanel scoreboardView;
 	private static JButton startButton;
-	private static JButton scoreButton;
 
 	public static View init(BlockingQueue<Message> queue) {
 		return new View();
@@ -38,16 +36,10 @@ public class View extends JFrame{
 		startButton.setPreferredSize(new Dimension(WelcomeAnimation.ICON_WIDTH, 40));
 		startButton.addActionListener(event -> startGame());
 		
-		scoreButton = new JButton("Scoreboard");
-		scoreButton.setPreferredSize(new Dimension(WelcomeAnimation.ICON_WIDTH, 40));
-		scoreButton.addActionListener(event -> startScoreboard());
-		
 		gameView = new GameView();
-		scoreboardView = new Scoreboard();
 
 		frame.add(welcomeAnim, BorderLayout.NORTH);
-		frame.add(startButton, BorderLayout.CENTER);
-		frame.add(scoreButton, BorderLayout.SOUTH);
+		frame.add(startButton, BorderLayout.SOUTH);
 		
 		// housekeeping to setup the JFrame window		
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -64,16 +56,6 @@ public class View extends JFrame{
 		frame.getContentPane().validate();
 	}
 	/**
-	 * handles drawing the scoreboard.
-	 */
-	public static void startScoreboard() {
-		frame.getContentPane().removeAll();
-		scoreboardView = new Scoreboard();
-		frame.getContentPane().add(scoreboardView);
-		frame.getContentPane().invalidate();
-		frame.getContentPane().validate();
-	}
-	/**
 	 * handles going back to and redrawing the welcome screen.
 	 */
 	public static void returnToWelcome() {
@@ -83,12 +65,8 @@ public class View extends JFrame{
 		startButton = new JButton("Start");
 		startButton.setPreferredSize(new Dimension(WelcomeAnimation.ICON_WIDTH, 40));
 		startButton.addActionListener(event -> startGame());
-		scoreButton = new JButton("Scoreboard");
-		scoreButton.setPreferredSize(new Dimension(WelcomeAnimation.ICON_WIDTH, 40));
-		scoreButton.addActionListener(event -> startScoreboard());
 		frame.getContentPane().add(welcomeAnim, BorderLayout.NORTH);
-		frame.getContentPane().add(startButton, BorderLayout.CENTER);
-		frame.getContentPane().add(scoreButton, BorderLayout.SOUTH);
+		frame.getContentPane().add(startButton, BorderLayout.SOUTH);
 		frame.getContentPane().invalidate();
 		frame.getContentPane().validate();
 	}
